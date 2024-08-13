f5659a4c1b42f1e862f2e4b7d2c175ef6472b7d2
tapyu <rubem.engenharia@gmail.com>
2024-08-13 09:57:19 -0300

    add dataset

--- Main.m
+++ Main.m
@@ -29,18 +29,21 @@ userInput.frequencyNo = 3;
 %% classes
 
 % scintillation classes
-K = 3; % number of classes
+K = 3; % number of S4/tau0 classes
 S4s = [0.5, 1, 0.9];
 tau0s = [0.7, 2, 0.25];
 
-scint_dataset = containers.Map('KeyType', 'char', 'ValueType', 'any');
+rxvel = [0 100]; % receiver velocity
+
+% list of field names for output
+fields = {'S4_050_tau0_070_rxvel_static', 'S4_100_tau0_200_rxvel_static', 'S4_090_tau0_025_rxvel_static', 'S4_050_tau0_070_rxvel_dynamic', 'S4_100_tau0_200_rxvel_dynamic', 'S4_090_tau0_025_rxvel_dynamic'};
 
 %% Start simulation
-for rxvel = [0 100]
+for v = 1:numel(rxvel)
     %    V1 = east-west velocity on the earth arc (m/s, eastward +)
     %    V2 = north-south velocity on the earch arc (m/s, northward +)
     %    V3 = up-down velocity (m/s, up +)
-    userInput.RXVel = [rxvel 0 0]';
+    userInput.RXVel = [rxvel(v) 0 0]';
     for k = 1:K % for all classes
         userInput.S4 = S4s(k); % S4 index (0~1)
         userInput.tau0 = tau0s(k); % Signal intensity decorrelation time in sec.
@@ -61,6 +64,7 @@ for rxvel = [0 100]
                     if userInput.dateTime(4) < 24 % hour
                         userInput.dateTime(4) = userInput.dateTime(4) + 1;
                     else
+                        userInput.dateTime(4) = 0;
                         if userInput.dateTime(3) < 29 % day
                             userInput.dateTime(3) = userInput.dateTime(3) + 1;
                         else
@@ -72,7 +76,7 @@ for rxvel = [0 100]
                                if userInput.dateTime(1) <= year(datetime('now')) % year
                                    userInput.dateTime(1) = userInput.dateTime(1) + 1;
                                else
-                                   newME = MException('Additional:ErrorOccurred', 'All years were tried, but without success');
+                                   newME = MException('Additional:ErrorOccurred', 'All years, days, months, and hours were tried, but without success.');
                                    newME = newME.addCause(ME);
                                    rethrow(newME);
                                end
@@ -95,8 +99,10 @@ for rxvel = [0 100]
         end
         %% Generate scintillation signal field realizations
         [Scin_psi, Scin_amp, Scin_phi] = RunGenScintFieldRealization(userInput,satGEOM,U_mapped,rhoFVeff_mapped);
-        scint_dataset(['S4_' num2str(userInput.S4) ...
-                       '_tau0_' num2str(userInput.tau0) ...
-                       '_rxvel_', strrep(num2str(userInput.RXVel'), '  ', '_')]) = [Scin_amp, Scin_phi];
+        scint_dataset.(fields{k + (v-1)*K}) = cat(1, reshape(Scin_amp, [1 size(Scin_amp)]), reshape(Scin_phi, [1 size(Scin_phi)]));
     end
 end
+
+scint_dataset.sampling_time = .01; % check `Dt` in Libraries/GenScintFieldRealization/RunGenScintFieldRealization.m
+
+save('scint_dataset.mat', 'scint_dataset');
